ee8c2e07cefebbb9766151aae13d2ec5d0c4d37e
morabrandoi <morabrandoi@gmail.com>
2023-01-10 01:49:41 -0500

    added cmake

---
+++ CMakeFiles/AnomalyDetector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AnomalyDetector"
+  "AnomalyDetector.pdb"
+  "CMakeFiles/AnomalyDetector.dir/main.cpp.o"
+  "CMakeFiles/AnomalyDetector.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnomalyDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
